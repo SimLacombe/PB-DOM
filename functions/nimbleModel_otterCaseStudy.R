0d19049461414abc03803ba0049f2cd61debce86
SimLacombe <simon.lacombe0@gmail.com>
2026-01-28 14:26:33 +0100

    added scripts for the otter case study

---
+++ functions/nimbleModel_otterCaseStudy.R
@@ -0,0 +1,81 @@
+require(nimble)
+
+myModel <- nimbleCode({
+  
+  ### PRIORS ###
+  
+  # Intercepts
+  alpha_gam   ~ dnorm(0, 1/2.5^2)         # colonization intercept
+  alpha_psi   ~ dnorm(0, 1/2.5^2)         # initial occu intercept
+  alpha_omega ~ dnorm(0, 1/2.5^2)         # extinction intercept
+  alpha_rho[1] ~ dnorm(0, 1/2.5^2)       # detection intercept for opport visits
+  alpha_rho[2] ~ dnorm(0, 1/2.5^2)       # detection intercept for stdzed data
+  
+  # Slopes 
+  for(c in 1:ncovs_gamma){
+    beta[c] ~ dnorm(0, 1/2.5^2)
+  }
+  beta_rho_t[1] ~ dnorm(0, 1/2.5^2)  # detection time trend for opport visits
+  beta_rho_t[2] ~ dnorm(0, 1/2.5^2)  # detection time trend for stdzed visits
+  
+  # Scale and intensity parameters of the colonisation kernel 
+  sigma ~ dlnorm(log(30), 1)       
+  alpha ~ dlnorm(log(0.5), 1)
+  
+  ### ECOLOGICAL MODEL ###
+  
+  # loop over sites
+  for (i in 1:nSites) {
+    # first year
+    z[1, i] ~ dbern(psi)
+    
+    # following years
+    for (t in 2:nSeasons) {
+        # loop over the neighbors of i
+        for (j in (dmatP[i] + 1):dmatP[i + 1]) {
+          delta[t - 1, j] <- exp(-d2[j] / (2 * sigma ** 2)) * z[t - 1, dmatI[j]]
+        }
+        gamma_ti[t - 1, i] <- gamma[i] * (1 - exp(- 100 * alpha/ (2 * pi * sigma ** 2) * sum(delta[t - 1, (dmatP[i] + 1):dmatP[i + 1]])))
+        z[t, i] ~ dbern(z[t - 1, i] * (1 - omega) + (1 - z[t - 1, i]) * gamma_ti[t - 1, i])
+      }
+    }
+  
+  ### OBSERVATION MODEL ###
+  
+  for (i in 1:nSites) {
+    for (t in 1:nSeasons) {
+      for (k in 1:nSurveys){
+        y[t, i, k] ~ dbern(prob = z[t, i] * rho[t, i, k])
+      }
+    }
+  }
+  
+  ### LOGIT LINEAR PREDICTORS ###
+  
+  omega <- ilogit(alpha_omega)
+  psi <- ilogit(alpha_psi)
+
+  for (i in 1:nSites) {
+    gamma[i] <- ilogit(alpha_gam + inprod(beta[1:ncovs_ecol], X[i, 1:ncovs_ecol]))
+    for (t in 1:nSeasons) {
+      for (k in 1:nSurveys){
+        rho[t, i, k] <- ilogit(alpha_rho[effort[year[t,k], i] + 1] +
+                                 beta_rho_t[effort[year[t,k], i] + 1] * year[t,k])
+      }
+    }
+  }
+})
+
+initial.values <- function(zst, ncovs_ecol, ncovs_det, b, lambda) {
+  list(
+    z = zst,
+    alpha_psi = rnorm(1, 0, 1),
+    alpha_gam = rnorm(1, 0, 1),
+    alpha_omega = rnorm(1, 0, 1),
+    alpha_rho = rnorm(2, 0, 1), 
+    beta = rnorm(ncovs_ecol, 0, 1),
+    beta_rho_t = rnorm(2, 0, 1),
+    sigma = rlnorm(1, log(30), 1),
+    alpha = rlnorm(1, log(0.2), 1)
+  )
+}
---
+++ otter_case_study/src/fit_otter_case_study.R
@@ -0,0 +1,119 @@
+library(tidyr)
+library(dplyr)
+library(purrr)
+library(sf)
+library(nimble)
+
+rm(list = ls())
+
+source("functions/utilities.R")
+
+filenames <- list(loutres = "otter_case_study/data/v_ref_presence_loutre_par_an_2000_25_utf8.csv",
+                  bg = "otter_case_study/data/background_grid.gpkg",
+                  eff = "otter_case_study/data/effort.rds")
+
+annees <- 2000:2023
+thr <- 50
+
+gamma_covs <- c("riverLength","ripForest", "CYP", "ANG", "ECR", "SAL")
+
+### 1. DONNEES  ----------------------------------------------------------------
+
+donnees_loutres <- read.csv(filenames$loutres, sep = ";") %>%
+  filter(annee %in% annees) %>%
+  rename(n = nombre.de.données.dans.la.maille)
+donnees_loutres <- st_as_sf(donnees_loutres, wkt = "wkt_geom_4326", crs = 4326) %>%
+  st_transform(crs = 2154)
+
+bg <- read_sf(filenames$bg) %>%
+  arrange(id) %>%
+  filter(cellArea >= 3.33e07)
+
+effort <- readRDS(filenames$eff)
+
+### 2. FORMATAGE DES DONNEES ---------------------------------------------------
+
+K <- 4
+N <- nrow(bg)
+T <- length(annees) / K
+
+y_df <- split(st_centroid(donnees_loutres), donnees_loutres$annee) %>%
+  map2(annees, ~ mutate(st_join(bg, .x), annee = .y)) %>%
+  reduce(rbind) %>%
+  st_drop_geometry %>%
+  mutate(t = (annee - annees[1]) %/% K + 1,
+         k = (annee - annees[1]) %% K + 1,
+         y = sign(replace_na(n, 0))) %>%
+  select(id, t, k, y) %>%
+  arrange(id)
+
+y <- array(y_df$y, dim = c(K, T, N)) %>%
+  aperm(c(2, 3, 1))
+
+d <- st_distance(st_centroid(bg))
+d <- matrix(as.numeric(d)/1000, N, N)
+d_sparse <- getSparse(d, thr = thr)
+rm(d)
+
+bg <- bg %>% 
+  mutate(across(all_of(c("ANG", "CYP", "ECR", "SAL")),
+                ~ log(.x - floor(min(.x)*100)/100))) %>%
+  mutate(across(all_of(gamma_covs), ~ c(scale(.x)))) %>%
+  arrange(id)
+
+yearMat <- matrix(1:(T*K), T, K, byrow = TRUE)
+  
+### 3. Fit NIMBLE --------------------------------------------------------------
+
+chain <- floor(runif(1, 100000, 999999)) #get Unique identifier for the MCMC chain
+cat(chain, "\n")
+dir.create(paste0("out/CHAIN_", chain))
+params <- list(ecol_covs, det_covs, bg)
+save(params, file=paste0("out/CHAIN_", chain, "/params.RData"))
+
+source("functions/nimbleModel_otterCaseStudy.R")
+
+my.constants <- list(nSites = N, 
+                     nSeasons = T,
+                     nSurveys = K, 
+                     ncovs_gamma = length(gamma_covs),
+                     X = as.matrix(st_drop_geometry(bg[,gamma_covs])),
+                     year = yearMat - floor(median(yearMat)),
+                     effort = effort,
+                     dmatP = d_sparse$p,
+                     dmatI = d_sparse$i ,
+                     d2 = d_sparse$d**2,
+                     pi = pi)
+
+mod <- nimbleModel(
+  code = myModel,
+  data = list(y = y),
+  constants = my.constants,
+  inits = initial.values(zst = array(1, dim = c(T, N)),
+                         ncovs_ecol = my.constants$ncovs_ecol,
+                         ncovs_det = my.constants$ncovs_det,
+                         b = c(splines$jags.ini[["b"]]),
+                         lambda = splines$jags.ini$lambda),
+  calculate = FALSE
+)
+
+Cmod <- compileNimble(mod)
+mod.Conf <- configureMCMC(mod, enableWAIC = FALSE)
+mod.Conf$addMonitors("z")
+
+mod.MCMC <- buildMCMC(mod.Conf, useConjugacy = FALSE)
+Cmod.MCMC <- compileNimble(mod.MCMC, project = mod)
+
+Cmod.MCMC$run(niter = 5000, nburnin = 5000, reset = TRUE)
+
+nChunks <- 5
+nIter <- 500
+for(c in 1:nChunks){
+  cat("Running chunk", c, "of", nChunks, "\n")
+  Cmod.MCMC$run(niter = nIter, nburnin = 0, reset = FALSE, resetMV = TRUE)
+  samples <- as.matrix(Cmod.MCMC$mvSamples)
+  saveRDS(samples,
+          file = paste0("otter_case_study/out/CHAIN_", chain, "/MCMC_samples_otter_model_chunk", c, ".rds"))
+  rm(samples)
+  gc()
+}
