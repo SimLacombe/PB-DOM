1ae878874acee800a8d3956d796c0a33577ce275
SimLacombe <simon.lacombe0@gmail.com>
2026-06-17 09:54:23 +0200

    add scripts to gitignore

--- functions/load_chain.R
+++ functions/load_chain.R
@@ -1,8 +1,8 @@
-load_chain <- function(path, T, N, get_z = TRUE){
+load_chain <- function(path, T, N, z_mean = TRUE){
   out <- map(list.files(path, full.names = TRUE, pattern = "MCMC"), readRDS) %>% 
     reduce(rbind)
   zm <- NULL
-  if(get_z){
+  if(z_mean){
     zm <- apply(out[, grepl("z\\[", colnames(out))], 2, mean) %>%
       matrix(nrow = T, ncol = N)
     out <- out[, !grepl("z\\[", colnames(out))]
